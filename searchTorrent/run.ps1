6a5f94d318490e6977c667cf492db03dd909ce28
rszladek <romain.szladek@orange.fr>
2018-04-04 19:27:37 +0200

    run file update series

--- searchTorrent/run.ps1
+++ searchTorrent/run.ps1
@@ -14,10 +14,8 @@ $result = @()
 
 # Set data
 $data = @(
-    @{"Name" = "dc-s-legends-of-tomorrow"; "Season" = "02"; "StartEpisode"= 1},
-    @{"Name" = "s-w-a-t"; "Season" = "01"; "StartEpisode"= 1},
-    @{"Name" = "the-flash"; "Season" = "04"; "StartEpisode"= 9},
-    @{"Name" = "arrow"; "Season" = "06"; "StartEpisode"= 9}
+    @{"Name" = "dc-s-legends-of-tomorrow"; "Season" = "02"; "StartEpisode"= 16},
+    @{"Name" = "s-w-a-t"; "Season" = "01"; "StartEpisode"= 12},
 )
  
 # Searching for torrent files
